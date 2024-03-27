19acda299a5fec660f33bd56d4f89ead6d2c9e5b
Kdevaulo <kdevaulo@gmail.com>
2024-03-27 19:14:19 +0500

    [UI] Top board user interface added

--- Assets/Kdevaulo/Scripts/TopBoardBehaviour/TopBoardView.cs
+++ Assets/Kdevaulo/Scripts/TopBoardBehaviour/TopBoardView.cs
@@ -1,7 +1,15 @@
-﻿namespace Kdevaulo.Scripts.TopBoardBehaviour
+﻿using TMPro;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kdevaulo.SpaceInvaders.TopBoardBehaviour
 {
-    public class TopBoardView
+    [AddComponentMenu(nameof(TopBoardView) + " in " + nameof(TopBoardBehaviour))]
+    public sealed class TopBoardView : MonoBehaviour
     {
-        
+        [SerializeField] private Button _pauseButton;
+
+        [SerializeField] private TextMeshProUGUI _scoreHolder;
     }
 }
